b7ae30f52238d597f72624235e7d9f8d935db1d0
Gareth Miller <gmiller290488@gmail.com>
2017-09-25 16:58:01 +0100

    Added a few more clauses to Concat script

--- concat.exs
+++ concat.exs
@@ -1,9 +1,16 @@
 defmodule Concat do
-	def join(a,b, sep \\ " ") do
-  		a <> sep <> b
+	def join(a, b \\ nil, sep \\ " ")
+
+	def join(a,b, _sep) when is_nil(b) do
+  		a 
+	end
+
+	def join(a, b, sep) do
+		a <> sep <> b
 	end
 end
 
-IO.puts Concat.join("hello", "world") # Hello world
-IO.puts Concat.join("hello", "world", "_") # Hello world
 
+IO.puts Concat.join("hello", "world")		# hello world
+IO.puts Concat.join("hello", "world", "_")	# hello_world
+IO.puts Concat.join("hello")  			# hello
