74d419a830a1f049a488954b0addb8ae9e787cf3
Gareth Miller <gmiller290488@gmail.com>
2017-09-25 17:08:23 +0100

    added a sum_list function to Math.exs

--- math.exs
+++ math.exs
@@ -6,5 +6,15 @@ defmodule Math do
 	defp do_sum(a,b) do
 		a + b
 	end
+
+	def sum_list([head | tail], accumulator) do
+		sum_list(tail, head + accumulator)
+	end
+
+	def sum_list([], accumulator) do
+		accumulator
+	end
 end
+
+IO.puts Math.sum_list([1, 2, 3], 0)
 
